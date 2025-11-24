48078129daccbc299a0fe81eb854835994e93b1d
VirajBhidola <virajbhidola2004@gmail.com>
2025-11-24 10:43:55 +0530

    feat: add basic python app with add and multiply functions

---
+++ app/main.py
@@ -0,0 +1,10 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+if __name__ == "__main__":
+    x, y = 2, 3
+    print("Addition:", add(x, y))
+    print("Multiplication:", multiply(x, y))
